af7481f5d083e700050860351892d97540e8e733
Matthew Lemon <lemon@matthewlemon.com>
2020-01-05 14:42:14 +0000

    quick fix with spurious characters getting into config

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; contents over https.
 ;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
 ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-nn;; this is also included in the configuration.org file
+;; this is also included in the configuration.org file
 (if (and (version< emacs-version "26.3") (>= libgnutls-version 30604))
     (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
