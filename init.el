0fe9ea3b7523241a8ec244d659e5d85fcda4bc2b
Matthew Lemon <lemon@matthewlemon.com>
2020-01-05 16:08:32 +0000

    gotest installed

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; contents over https.
 ;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
 ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-nn;; this is also included in the configuration.org file
+;; this is also included in the configuration.org file
 (if (and (version< emacs-version "26.3") (>= libgnutls-version 30604))
     (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
@@ -734,6 +734,8 @@ nn;; this is also included in the configuration.org file
   :commands (lsp lsp-deferred)
   :config
   (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
+
+(use-package gotest)
 
 ;; lsp-ui
 (use-package lsp-ui
