d780a096a192bb63f35a31663b2aaaee971fd616
Matthew Lemon <lemon@matthewlemon.com>
2021-10-18 15:01:48 +0100

    changes

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - x220 at <2021-10-15 Fri 17:33>
-(setq org-clock-stored-history '(("/home/lemon/org/archive.org" . 24934) ("/home/lemon/org/archive.org" . 54821) ("/home/lemon/org/work.org" . 1528) ("/home/lemon/org/work.org" . 5339) ("/home/lemon/org/habits.org" . 2302)))
+;; org-clock-save.el - x220 at <2021-10-18 Mon 11:41>
+(setq org-clock-stored-history '(("/home/lemon/org/archive.org" . 24934) ("/home/lemon/org/archive.org" . 54821) ("/home/lemon/org/work.org" . 1486) ("/home/lemon/org/work.org" . 5297) ("/home/lemon/org/home.org" . 2738)))
