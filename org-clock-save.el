629e8d144ccdb850160169287ca3fb7976336d2d
Matthew Lemon <matt@matthewlemon.com>
2022-11-18 16:52:51 +0000

    clock history

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - archdesk at <2022-10-28 Fri 14:51>
+;; org-clock-save.el - archdesk at <2022-11-18 Fri 09:17>
 (setq org-clock-stored-history '(("/home/lemon/org/archive.org" . 24934) ("/home/lemon/org/archive.org" . 61601) ("/home/lemon/org/work.org" . 1) ("/home/lemon/org/home.org" . 1)))
