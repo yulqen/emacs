626bedc4c72e6f9c19aaabc567ae495265456e9d
Matthew Lemon <matt@matthewlemon.com>
2022-11-18 16:50:21 +0000

    Liberation Mono now on Arch

--- init.el
+++ init.el
@@ -27,6 +27,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(custom-safe-themes
+   '("3d2e532b010eeb2f5e09c79f0b3a277bfc268ca91a59cdda7ffd056b868a03bc" default))
  '(display-line-numbers-type 'relative)
  '(display-time-mode t)
  '(package-selected-packages
@@ -37,4 +39,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack Nerd Font" :foundry "SRC" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 113 :width normal)))))
