5d2075e0e9c5fd6287af88f7898b58d198667c9d
Matthew Lemon <matt@matthewlemon.com>
2022-11-21 06:35:47 +0000

    formatting

--- init.el
+++ init.el
@@ -164,7 +164,7 @@
  ;; encoding
  (prefer-coding-system 'utf-8)
  (setq coding-system-for-read 'utf-8)
-(setq coding-system-for-write 'utf-8)
+ (setq coding-system-for-write 'utf-8)
 
 ;; calfw
 (use-package calfw-org
