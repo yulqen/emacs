cd28f219f934cdf2e09d47343ba62a94b7b0ff44
Matthew Lemon <matt@matthewlemon.com>
2022-11-22 12:00:34 +0000

    more key stuff for helm

--- init.el
+++ init.el
@@ -237,7 +237,9 @@ Restart works only on graphic display."
   (require 'helm-config)
   (helm-mode 1))
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
