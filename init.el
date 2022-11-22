7e172ae262bdac527acfa388c3c9ebca1f216298
Matthew Lemon <matt@matthewlemon.com>
2022-11-22 11:49:08 +0000

    added a new helm keybinding (for bookmarks)

--- init.el
+++ init.el
@@ -237,6 +237,7 @@ Restart works only on graphic display."
   (require 'helm-config)
   (helm-mode 1))
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
