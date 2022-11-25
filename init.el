dbbfbe79ac164f00fee6a3e64e17682ac2867f03
Matthew Lemon <matt@matthewlemon.com>
2022-11-25 06:52:48 +0000

    added embark and marginalia

--- init.el
+++ init.el
@@ -225,6 +225,41 @@ Restart works only on graphic display."
 
 ;; PACKAGES
 
+;; embark
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+
+;; markdown
 (use-package markdown-mode
   :ensure t
   :bind (:map markdown-mode-map
