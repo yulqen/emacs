a3e02dc48e58d25c150bafdc018edd89e552f209
Matthew Lemon <matt@matthewlemon.com>
2022-12-12 08:23:22 +0000

    set ef-spring theme

--- init.el
+++ init.el
@@ -281,7 +281,7 @@ Restart works only on graphic display."
   (mapc #'disable-theme custom-enabled-themes)
 
   ;; Load the theme of choice:
-  (load-theme 'ef-summer :no-confirm)
+  (load-theme 'ef-spring :no-confirm)
   ;; OR use this to load the theme which also calls `ef-themes-post-load-hook':
   ;;(ef-themes-select 'ef-summer)
 
