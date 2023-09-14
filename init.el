29350057d2c03a8d8b2bfef2e36dbce414f50df7
Matthew Lemon <y@yulqen.org>
2023-09-14 19:49:39 +0100

    Brought back in notmuch email

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-.el --- emacs configuration. -*- lexical-binding: t -*-
+;;; --- emacs configuration. -*- lexical-binding: t -*-
 ;;; package --- MR Lemon emacs config
 ;;; Commentary:
 ;;; basic config
@@ -102,7 +102,7 @@ Restart works only on graphic display."
     (mrl/stop-emacs-1)))
 
 ;; notmuch is apparently already installed with notmuch from arch
-;;(require 'notmuch)
+(require 'notmuch)
 
 ;; load my crap
 (add-to-list 'load-path "~/.config/emacs/lisp")
@@ -665,75 +665,75 @@ Restart works only on graphic display."
 ;;   :ensure t)
 
 ;; notmuch
-; (use-package notmuch
-;   :defer t
-;   :config
-;   (define-key notmuch-show-mode-map "S"
-;     (lambda ()
-;       "mark message as spam"
-;       (interactive)
-;       (notmuch-show-tag (list "+spam" "-inbox"))))
-;   (define-key notmuch-search-mode-map "S"
-;     (lambda ()
-;       "mark message as spam"
-;       (interactive)
-;       (notmuch-search-tag (list "+spam" "-inbox"))))
-;   (define-key notmuch-search-mode-map "d"
-;     (lambda ()
-;       "toggle deleted tag for message"
-;       (interactive)
-;       (if (member "deleted" (notmuch-search-get-tags))
-;           (notmuch-search-tag (list "-deleted"))
-;         (progn
-;           (notmuch-search-tag (list "-unread"))
-;           (notmuch-search-tag (list "-new"))
-;           (notmuch-search-tag (list "+deleted"))))))
-;   (setq send-mail-function 'sendmail-send-it
-;         notmuch-search-result-format '(("date" . "%12s ")
-;                                        ("count" . "%7s ")
-;                                        ("authors" . "%-20s ")
-;                                        ("subject" . "%-80s ")
-;                                        ("tags" . "(%s) "))
-;         sendmail-program "/usr/bin/msmtp"
-;         notmuch-archive-tags '("-inbox" "+archived" "-new")
-;         message-kill-buffer-on-exit t
-;         notmuch-draft-folder "fastmail/Drafts"
-;         notmuch-fcc-dirs "fastmail/Sent +sent -unread -inbox"
-;         notmuch-search-oldest-first nil
-;         mail-specify-envelope-from t
-;         message-signature "\n-- \nMatthew"
-;         mm-text-html-renderer 'lynx
-;         message-sendmail-envelope-from 'header
-;         mail-envelope-from 'header
-;         notmuch-saved-searches '((:name "Inbox"
-;                                         :query "tag:inbox"
-;                                         :count-query "tag:inbox and tag:unread"
-;                                         :sort-order newest-first
-;                                         :key "i")
-;                                  (:name "Todo"
-;                                         :query "tag:todo"
-;                                         :sort-order newest-first
-;                                         :key "t")
-;                                  (:name "Unread"
-;                                         :query "tag:unread"
-;                                         :sort-order newest-first
-;                                         :key "u")
-;                                  (:name "Sent"
-;                                         :query "tag:sent"
-;                                         :sort-order newest-first
-;                                         :key "s")
-;                                  (:name "All Mail"
-;                                         :query "*"
-;                                         :sort-order newest-first
-;                                         :key "a")
-;                                  (:name "School"
-;                                         :query "tag:school"
-;                                         :sort-order newest-first
-;                                         :key "S")                                 
-;                                  (:name "Deleted"
-;                                         :query "tag:deleted"
-;                                         :sort-order newest-first
-;                                         :key "d"))))
+(use-package notmuch
+  :defer t
+  :config
+  (define-key notmuch-show-mode-map "S"
+    (lambda ()
+      "mark message as spam"
+      (interactive)
+      (notmuch-show-tag (list "+spam" "-inbox"))))
+  (define-key notmuch-search-mode-map "S"
+    (lambda ()
+      "mark message as spam"
+      (interactive)
+      (notmuch-search-tag (list "+spam" "-inbox"))))
+  (define-key notmuch-search-mode-map "d"
+    (lambda ()
+      "toggle deleted tag for message"
+      (interactive)
+      (if (member "deleted" (notmuch-search-get-tags))
+          (notmuch-search-tag (list "-deleted"))
+        (progn
+          (notmuch-search-tag (list "-unread"))
+          (notmuch-search-tag (list "-new"))
+          (notmuch-search-tag (list "+deleted"))))))
+  (setq send-mail-function 'sendmail-send-it
+        notmuch-search-result-format '(("date" . "%12s ")
+                                       ("count" . "%7s ")
+                                       ("authors" . "%-20s ")
+                                       ("subject" . "%-80s ")
+                                       ("tags" . "(%s) "))
+        sendmail-program "/usr/bin/msmtp"
+        notmuch-archive-tags '("-inbox" "+archived" "-new")
+        message-kill-buffer-on-exit t
+        notmuch-draft-folder "purelymail/Drafts"
+        notmuch-fcc-dirs "purelymail/Sent +sent -unread -inbox"
+        notmuch-search-oldest-first nil
+        mail-specify-envelope-from t
+        message-signature "\n-- \nMatthew"
+        mm-text-html-renderer 'lynx
+        message-sendmail-envelope-from 'header
+        mail-envelope-from 'header
+        notmuch-saved-searches '((:name "Inbox"
+                                        :query "tag:inbox"
+                                        :count-query "tag:inbox and tag:unread"
+                                        :sort-order newest-first
+                                        :key "i")
+                                 (:name "Todo"
+                                        :query "tag:todo"
+                                        :sort-order newest-first
+                                        :key "t")
+                                 (:name "Unread"
+                                        :query "tag:unread"
+                                        :sort-order newest-first
+                                        :key "u")
+                                 (:name "Sent"
+                                        :query "tag:sent"
+                                        :sort-order newest-first
+                                        :key "s")
+                                 (:name "All Mail"
+                                        :query "*"
+                                        :sort-order newest-first
+                                        :key "a")
+                                 (:name "School"
+                                        :query "tag:school"
+                                        :sort-order newest-first
+                                        :key "S")                                 
+                                 (:name "Deleted"
+                                        :query "tag:deleted"
+                                        :sort-order newest-first
+                                        :key "d"))))
 
 ;; calfw
 ;; (use-package calfw-org
@@ -1373,9 +1373,9 @@ If failed try to complete the common part with `company-complete-common'"
           (tags-todo "-SCHEDULED>=\"<today>\"&TODO=\"NEXT\""
                      ((org-agenda-overriding-header "Home NEXT UNSCHEDULED")
                       (org-agenda-sorting-strategy '(alpha-up deadline-down scheduled-down priority-down))))
-          (tags-todo "TODO=\"PROJECT\"" ((org-agenda-overriding-header "Projects")
-                                         (org-agenda-sorting-strategy '(alpha-up))))
           (tags "idea" ((org-agenda-overriding-header "Ideas")
+                                         (org-agenda-sorting-strategy '(alpha-up))))
+          (tags-todo "TODO=\"PROJECT\"" ((org-agenda-overriding-header "Projects")
                                          (org-agenda-sorting-strategy '(alpha-up))))
           (tags-todo "TODO=\"NEXT\"" ((org-agenda-overriding-header "All Next Actions")
                                       (org-agenda-sorting-strategy '(alpha-up deadline-down scheduled-down priority-down))))
