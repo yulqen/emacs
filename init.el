96d60ccf36ab3ef80cd5e65c95fabf20340fd28d
Matthew Lemon <y@yulqen.org>
2024-07-10 18:55:46 +0100

    Reduce size of vertico window and stop it resizing

--- init.el
+++ init.el
@@ -521,11 +521,12 @@ Restart works only on graphic display."
   ;;Different scroll margin
   (setq vertico-scroll-margin 0)
 
-  ;;Show more candidates
-  (setq vertico-count 20)
+  ;;Show more candidates - we don't need many
+  (setq vertico-count 10)
 
-  ;;Grow and shrink the Vertico minibuffer
-  (setq vertico-resize t)
+  ;;Grow and shrink the Vertico minibuffer - set to nil because it
+  ;; jumping up and down is nippy
+  (setq vertico-resize nil)
 
   ;;Optionally enable cycling for `vertico-next' and `vertico-previous'.
   (setq vertico-cycle t))
