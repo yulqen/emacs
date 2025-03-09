270ae4ec9c9167ad335735e9be1c2c59ec7651f4
Matthew Lemon <y@yulqen.org>
2025-03-09 20:49:17 +0000

    Brings back x clipboard integration

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
 
 ;; Don't save the clipboard as this tends to hang
 (setq x-select-enable-clipboard-manager nil)
-(setq select-enable-clipboard nil)
+;;(setq select-enable-clipboard nil)
 
 ;; easy revert buffer, which I never use
 (global-set-key (kbd "C-c R") (lambda () (interactive) (revert-buffer t t)))
