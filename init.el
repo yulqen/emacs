a29b1f52dd401e2c8643d1a4791bcb9ebb1cc41e
Matthew Lemon <y@yulqen.org>
2025-03-15 19:44:39 +0000

    Adds a function to duplicate line

--- init.el
+++ init.el
@@ -24,6 +24,21 @@
 ;; Use rc.el from rexim
 (load "~/.emacs.d/lisp/rc.el")
 
+
+(defun rc/duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (point-at-bol)))
+        (line (let ((s (thing-at-point 'line t)))
+                (if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (forward-char column)))
+
+(global-set-key (kbd "C-,") 'rc/duplicate-line)
+
 ;; ensure theme
 (rc/require-theme 'gruber-darker)
 
