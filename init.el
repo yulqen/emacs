a9147b647076c2c2506435fa12a522893fa0d20b
Matthew Lemon <y@yulqen.org>
2025-03-16 17:15:37 +0000

    Changes to completion

--- init.el
+++ init.el
@@ -270,8 +270,44 @@
 
 ;; company mode
 (rc/require 'company)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 2)
+(setq company-show-numbers t)
+;; To prevent default down-casing.
+;; https://emacs.stackexchange.com/questions/10837/how-to-make-company-mode-be-case-sensitive-on-plain-text
+(setq company-dabbrev-downcase nil)
+;; 2023-01-13 From a Reddit post on mixed case issue.
+(setq company-dabbrev-ignore-case nil)
+(setq company-dabbrev-code-ignore-case nil)
 (add-hook 'after-init-hook 'global-company-mode)
 (setq company-global-modes '(not org-mode))
+
+;; Enable Completion Preview mode in code buffers
+(add-hook 'prog-mode-hook #'completion-preview-mode)
+;; also in text buffers
+(add-hook 'text-mode-hook #'completion-preview-mode)
+;; and in \\[shell] and friends
+(with-eval-after-load 'comint
+  (add-hook 'comint-mode-hook #'completion-preview-mode))
+
+(with-eval-after-load 'completion-preview
+  ;; Show the preview already after two symbol characters
+  (setq completion-preview-minimum-symbol-length 2)
+
+  ;; Non-standard commands to that should show the preview:
+
+  ;; Org mode has a custom `self-insert-command'
+  (push 'org-self-insert-command completion-preview-commands)
+  ;; Paredit has a custom `delete-backward-char' command
+  (push 'paredit-backward-delete completion-preview-commands)
+
+  ;; Bindings that take effect when the preview is shown:
+
+  ;; Cycle the completion candidate that the preview shows
+  (keymap-set completion-preview-active-mode-map "M-n" #'completion-preview-next-candidate)
+  (keymap-set completion-preview-active-mode-map "M-p" #'completion-preview-prev-candidate)
+  ;; Convenient alternative to C-i after typing one of the above
+  (keymap-set completion-preview-active-mode-map "M-i" #'completion-preview-insert))
 
 ;; direnv
 (rc/require 'direnv)
