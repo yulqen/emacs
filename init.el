6adbdad09dc8a6c56fce4eab1403eef17a912682
Matthew Lemon <y@yulqen.org>
2025-04-09 10:16:48 +0100

    Ensure emmet mode is switched on in web-mode

--- init.el
+++ init.el
@@ -83,6 +83,7 @@
 (rc/require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook 'emmet-mode) ;; try this to get emmet on web-mode
 
 ;; use mu4e for e-mail in emacs
 (setq mail-user-agent 'mu4e-user-agent)
