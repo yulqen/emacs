c915acf4f46105e80a8781f07a338f29759d2657
Matthew Lemon <y@yulqen.org>
2025-04-10 09:06:52 +0100

    Installed Marginalia

--- init.el
+++ init.el
@@ -190,6 +190,22 @@
   :config
   (setq web-mode-markup-indent-offset 2))
 
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
 (use-package elfeed
   :bind ("C-x w" . elfeed)
   :config
