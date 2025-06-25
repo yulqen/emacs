3b93c801e869eab6c8f8cef336c08274a5f392eb
Matthew Lemon <y@yulqen.org>
2025-06-26 00:58:07 +0100

    Removes out of data org-roam config

--- init.el
+++ init.el
@@ -275,25 +275,6 @@
   )
 
 
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory "~/Documents/org-roam/")
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
-  :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-completion-everywhere t)
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
-
 (use-package flycheck-clj-kondo
   :hook (after-init . global-flycheck-mode))
 
